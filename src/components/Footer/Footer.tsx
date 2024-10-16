c27cbf6170d11adac9152e06f21d2885c43b3bfa
Jesse <crayonfuse@tutanota.com>
2024-10-16 15:38:08 +0200

    added mobile footer for a centered view

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,35 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 right-0 z-10 flex flex-col items-end p-4 pb-20 space-y-2 bg-transparent text-dark-text-secondary md:pb-4">
-      <Link to="/impressum" className="text-sm transition-colors hover:text-dark-text">
-        Impressum
-      </Link>
-      <Link to="/cookies" className="text-sm transition-colors hover:text-dark-text">
-        Cookies
-      </Link>
-      <Link to="/privacy" className="text-sm transition-colors hover:text-dark-text">
-        Privacy
-      </Link>
-    </footer>
+    <>
+      {/* Mobile footer */}
+      <footer className="fixed bottom-0 left-0 right-0 z-10 flex items-center justify-center p-4 space-x-4 bg-dark-bg text-dark-text-secondary md:hidden">
+        <Link to="/impressum" className="text-sm transition-colors hover:text-dark-text">
+          Impressum
+        </Link>
+        <Link to="/cookies" className="text-sm transition-colors hover:text-dark-text">
+          Cookies
+        </Link>
+        <Link to="/privacy" className="text-sm transition-colors hover:text-dark-text">
+          Privacy
+        </Link>
+      </footer>
+
+      {/* Desktop footer */}
+      <footer className="fixed z-10 flex-col items-end hidden p-4 space-y-2 bottom-4 right-4 bg-dark-bg text-dark-text-secondary md:flex">
+        <Link to="/impressum" className="text-sm transition-colors hover:text-dark-text">
+          Impressum
+        </Link>
+        <Link to="/cookies" className="text-sm transition-colors hover:text-dark-text">
+          Cookies
+        </Link>
+        <Link to="/privacy" className="text-sm transition-colors hover:text-dark-text">
+          Privacy
+        </Link>
+      </footer>
+    </>
   );
 };
 
