81aeb49ce9f2505cba2f08b1ff88322fb6e5704e
Jesse <crayonfuse@tutanota.com>
2024-10-19 22:40:32 +0200

    changed some padding and centering in contact page, changed styling for mobile view in impressum page, made clock unselectable

--- src/pages/Legal/Impressum.tsx
+++ src/pages/Legal/Impressum.tsx
@@ -3,47 +3,42 @@ import React from "react";
 export function ImpressumPage() {
   return (
     <div className="min-h-screen py-8 pb-32 bg-dark-bg text-dark-text">
-      <div className="container max-w-3xl px-4 mx-auto sm:px-6 lg:px-8">
+      <div className="container max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
         <h1 className="mb-8 text-3xl font-bold text-center">Impressum</h1>
 
         <div className="space-y-6">
           <Section title="Kontakt">
-            <p>Niklas Decker Veranstaltungstechnik Dienstleister</p>
+            <p>Niklas Decker</p>
+            <p>Veranstaltungstechnik Dienstleister</p>
             <p>Auf der Platte 8</p>
             <p>88284 Wolpertswende</p>
             <p>Telefon: 017695449722</p>
-            <p>E-Mail: info@decker-eventtechnik.com</p>
+            <p>E-Mail: niki25.9@web.de</p>
           </Section>
 
           <Section title="Angaben zur Berufshaftpflicht">
-            <p>Name und Sitz des Versicherers:</p>
+            <p><strong>Name und Sitz des Versicherers:</strong></p>
             <p>Allianz SE</p>
             <p>Florian Kresser</p>
             <p>Rupert-Mayer-Straße 10</p>
             <p>88212 Ravensburg</p>
-          </Section>
 
-          <Section title="Geltungsraum der Versicherung">
+            <p><strong>Geltungsraum der Versicherung:</strong></p>
             <p>Deutschland</p>
+            <p>EU-Streitschlichtung</p>
           </Section>
 
-          <Section title="EU-Streitschlichtung">
-            <p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:</p>
+          <Section title="Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:">
             <p>
-              <a 
-                href="https://ec.europa.eu/consumers/odr/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="text-blue-400 underline hover:text-blue-300"
-              >
-                https://ec.europa.eu/consumers/odr/
+              <a href="https://ec.europa.eu/consumers/odr" className="text-blue-400 underline hover:text-blue-300">
+                https://ec.europa.eu/consumers/odr
               </a>
             </p>
             <p>Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
-          </Section>
-
-          <Section title="Verbraucherstreitbeilegung/Universalschlichtungsstelle">
-            <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+            <p>
+              Verbraucherstreitbeilegung/Universalschlichtungsstelle: Wir sind nicht bereit oder verpflichtet, an
+              Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+            </p>
           </Section>
         </div>
       </div>
@@ -61,3 +56,5 @@ function Section({ title, children }: { title: string; children: React.ReactNode
     </div>
   );
 }
+
+export default ImpressumPage;
