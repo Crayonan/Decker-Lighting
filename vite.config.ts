ea5df7e420f7ba0489933a982761f64e6c54040e
Crayonan <dev@cicd.host>
2024-10-29 08:00:57 +0100

    Removed some image loading

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,23 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['@tanstack/react-query']
+        }
+      }
+    }
+  }
 })
 
 
